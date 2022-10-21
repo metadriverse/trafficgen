10c4590885377e478632a3820a02c45fd56f2a32
Alan <fenglan18@outlook.com>
2022-10-21 14:33:38 +0200

    add setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,6 @@ from setuptools import setup
 
 requirements = open('requirements.txt').readlines()
 setup(
-    name='TrafficGen',
-    version='',
-    packages=['TrafficGen_act', 'TrafficGen_act.data_process', 'TrafficGen_init', 'TrafficGen_init.data_process'],
-    package_dir={'': 'trafficgen'},
-    url='',
-    license='',
-    author='fenglan',
-    author_email='',
-    description='',
+    name='trafficgen',
     install_requires = requirements
 )
