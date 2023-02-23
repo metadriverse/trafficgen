09f57d4bde0ac1b28fd3322be116b30797be39ae
Alan <fenglan18@outlook.com>
2023-02-23 19:35:11 +0100

    update script

--- utils/data_trans.sh
+++ utils/data_trans.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir $1/raw_{0..9}
-
-for i in 0 1 2 3 4 5 6 7 8 9; do
-     mv $1/training_20s.tfrecord-00${i}* $1/raw_${i}/
-done
-#
+#mkdir $1/raw_{0..9}
 #
 #for i in 0 1 2 3 4 5 6 7 8 9; do
-#    nohup python trans20.py /data/lanfeng/waymo/raw_${i} /data/lanfeng/waymo/init_data ${i} > ${i}.log 2>&1 &
+#     mv $1/training_20s.tfrecord-00${i}* $1/raw_${i}/
 #done
+
+mkdir $1/trafficgen_data
+for i in 0 1 2 3 4 5 6 7 8 9; do
+    nohup python utils/trans20.py $1/raw_${i} $1/trafficgen_data ${i} > ${i}.log 2>&1 &
+done
