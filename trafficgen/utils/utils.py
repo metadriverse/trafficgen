1a0e59e9246147436b8e33dd8a233745237870b3
Alan <fenglan18@outlook.com>
2023-03-03 17:55:11 +0100

    add seed

--- trafficgen/utils/utils.py
+++ trafficgen/utils/utils.py
@@ -2,7 +2,16 @@ import copy
 import numpy as np
 import torch
 from shapely.geometry import Polygon
+import random,os
 
+def setup_seed(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 def normalize_angle(angle):
     if isinstance(angle, torch.Tensor):
         while not torch.all(angle >= 0):
