c742792bd640ae310a8347c2a1d1d2f3bf96652e
Alan <fenglan18@outlook.com>
2023-05-07 20:27:43 +0200

    update model

--- trafficgen/init/model/tg_init.py
+++ trafficgen/init/model/tg_init.py
@@ -227,14 +227,13 @@ class initializer(pl.LightningModule):
         }
 
     def agent_feature_extract(self, agent_feat, agent_mask, random_mask):
-        agent = agent_feat[..., :-2].float()
+        agent = agent_feat[..., :-2]
         agent_line_type = agent_feat[..., -2].to(int)
         agent_line_traf = agent_feat[..., -1].to(int)
         # agent_line_traf = torch.zeros_like(agent_line_traf).to(agent.device)
 
-        agent_line_type_embed = self.type_embedding(agent_line_type).float()
-        agent_line_traf[:]=0
-        agent_line_traf_embed = self.traf_embedding(agent_line_traf).float()
+        agent_line_type_embed = self.type_embedding(agent_line_type)
+        agent_line_traf_embed = self.traf_embedding(agent_line_traf)
 
         min_agent_num = self.cfg['min_agent']
         if random_mask:
@@ -254,14 +253,13 @@ class initializer(pl.LightningModule):
 
     def map_feature_extract(self, lane_inp, line_mask, context_agent):
 
-        polyline = lane_inp[..., :4].float()
+        polyline = lane_inp[..., :4]
         polyline_type = lane_inp[..., 4].to(int)
         polyline_traf = lane_inp[..., 5].to(int)
         # polyline_traf = torch.zeros_like(polyline_traf).to(agent.device)
 
-        polyline_type_embed = self.type_embedding(polyline_type).float()
-        polyline_traf[:]=0
-        polyline_traf_embed = self.traf_embedding(polyline_traf).float()
+        polyline_type_embed = self.type_embedding(polyline_type)
+        polyline_traf_embed = self.traf_embedding(polyline_traf)
         # polyline_traf_embed = torch.zeros_like(polyline_traf_embed,device=device)
 
         # agent features
