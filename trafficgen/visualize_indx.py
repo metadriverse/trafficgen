a689a1abe8f60cbef6162f48bacd6877b8e386f2
Alan <fenglan18@outlook.com>
2023-06-04 01:20:13 +0200

    add vis indx

--- trafficgen/visualize_indx.py
+++ trafficgen/visualize_indx.py
@@ -3,6 +3,11 @@ import argparse
 from trafficgen.init.utils.init_dataset import WaymoAgent
 from trafficgen.traffic_generator.utils.tsne import visualize_tsne_points,visualize_tsne_images
 from trafficgen.traffic_generator.utils.vis_utils import draw
+import wandb
+
+wandb.init(
+    project="tsne",
+)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--idx', 'i', type=int)
@@ -20,5 +25,9 @@ for a in range(agent_num):
     agent_list.append(WaymoAgent(agent[[a]]))
 imag = draw(data['center'][0].numpy(), agent_list, other=data['rest'][0].numpy(), edge=data['bound'][0].numpy(),
      path=f'./img/{idx}.jpg', save=True)
-imag.show()
+
+ret = {}
+ret['idx'] = wandb.Image(imag)
+
+wandb.log(ret)
 
